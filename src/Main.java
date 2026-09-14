851ed37626ee58d802f2f37f1723b4fb75715beb
huen <ashtontehcreator@gmail.com>
2026-09-14 15:51:15 +0800

    Pushing main driver

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import HourlyEmployee.HourlyEmployee;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        HourlyEmployee houremp = new HourlyEmployee(101, "Alice Smith", 45.00f, 200.00);
+
+        System.out.println("---Hourly Employee Test--");
+        System.out.println("[displayHourlyEmployee()]");
+        houremp.displayHourlyEmployee();
+        System.out.println("\n\n[toString()]");
+        System.out.println(houremp.toString());
+
+        CommissionEmployee emp = new CommissionEmployee(301, "Jane Doe", 5000.00);
+
+        System.out.println("\n\n---Commission Employee Test---");
+        System.out.println("[displayCommissionEmployee()]");
+        emp.displayCommissionEmployee();
+
+        System.out.println("\n\n[toString()]");
+        System.out.println(emp);
     }
 }
+
